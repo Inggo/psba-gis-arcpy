e756e4ddff0f90f30b9973c04885828eb95084e9
Inggo PSBA <inggo@psba.edu>
2018-12-21 10:39:38 +0800

    Update HideShowLayers.py
    Strip invalid characters and add debug messages

--- scripts/HideShowLayers.py
+++ scripts/HideShowLayers.py
@@ -1,4 +1,5 @@
 import arcpy
+import re
 
 # Map document file - usually passed via File Iterator, pass as parameter 0
 file = arcpy.GetParameterAsText(0)
@@ -8,7 +9,7 @@ hideLayers = arcpy.GetParameterAsText(1).split("|")
 showLayers = arcpy.GetParameterAsText(2).split("|")
 # Save the output in a different filename (same folder) - pass as string in parameter 3;
 # if not set, will overwrite the original file
-saveAs = arcpy.GetParameterAsText(3)
+saveAs = re.sub(r'[\\/:"*?<>|]+', "", arcpy.GetParameterAsText(3))
 
 # Open map document
 mxd = arcpy.mapping.MapDocument(file)
@@ -17,10 +18,14 @@ mxd = arcpy.mapping.MapDocument(file)
 for lyr in arcpy.mapping.ListLayers(mxd):
 	# Hide layer if part of show list
 	if lyr.name in hideLayers:
+		arcpy.AddMessage("Hiding layer: " + lyr.name)
 		lyr.visible = False
 	# Show layer if part of hide list
 	elif lyr.name in showLayers:
 		lyr.visible = True
+		arcpy.AddMessage("Showing layer: " + lyr.name)
+	else:
+		arcpy.AddMessage("Ignoring layer: " + lyr.name)
 
 # Save the map document
 if saveAs:

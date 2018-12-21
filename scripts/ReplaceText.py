88181a73fdbe6a3ef9381f2d3e00c63b929476d8
Inggo Espinosa <inggo.espinosa@gmail.com>
2018-12-21 14:40:55 +0800

    Delete text if textToReplaceWith is blank

--- scripts/ReplaceText.py
+++ scripts/ReplaceText.py
@@ -4,7 +4,8 @@ import arcpy
 file = arcpy.GetParameterAsText(0)
 # Text to replace - pass as parameter 1
 textToReplace = arcpy.GetParameterAsText(1)
-# Text to replace with - pass as parameter 2
+# Text to replace with - pass as parameter 2;
+# if this is blank, remove the text element
 textToReplaceWith = arcpy.GetParametersAsText(2)
 
 # Open map document
@@ -12,7 +13,11 @@ mxd = arcpy.mapping.MapDocument(file)
 
 # Update all text elements
 for elm in arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT"):
-	elm.text = elm.text.replace(textToReplace, textToReplaceWith)
+	if textToReplace in elm.text:
+		if not textToReplaceWith:
+			elm.delete()
+		else:
+			elm.text = elm.text.replace(textToReplace, textToReplaceWith)
 
 # Save the map document
 mxd.save()
